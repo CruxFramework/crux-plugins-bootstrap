99d6f5c314e0dab5283a9b66d136c0610b7dec3c
wesley@triggolabs.com <wesley@triggolabs.com@e1a744db-34b1-0808-4b55-197acf403b58>
2014-06-02 18:46:10 +0000

    IGNORE Removing target folder

--- target/classes/org/cruxframework/crux/plugin/bootstrap/client/resource/css/v2/CssBootstrap2.java
+++
@@ -1,8 +0,0 @@
-package org.cruxframework.crux.plugin.bootstrap.client.resource.css.v2;
-
-import com.google.gwt.resources.client.CssResource;
-
-public interface CssBootstrap2 extends CssResource
-{
-
-}
--- target/classes/org/cruxframework/crux/plugin/bootstrap/client/resource/css/v3/CssBootstrapModal.java
+++
@@ -1,8 +0,0 @@
-package org.cruxframework.crux.plugin.bootstrap.client.resource.css.v3;
-
-import com.google.gwt.resources.client.CssResource;
-
-public interface CssBootstrapModal extends CssResource
-{
-
-}
